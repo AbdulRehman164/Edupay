1569082898a7624a62befd08877aafb68ca26f27
Abdul Rehman <abdulrehmang571@gmail.com>
2026-01-26 22:16:40 +0500

    added job cleanup config in bullmq

--- server/src/queues/index.js
+++ server/src/queues/index.js
@@ -2,7 +2,17 @@ import { Queue } from 'bullmq';
 
 export const queueName = 'generate-payslips';
 
-// basic connection - change host/port/password for your env
 const connection = { host: '127.0.0.1', port: 6379 };
 
-export const payslipQueue = new Queue(queueName, { connection });
+export const payslipQueue = new Queue(queueName, {
+    connection,
+    defaultJobOptions: {
+        removeOnComplete: {
+            age: 60 * 60,
+            count: 1000,
+        },
+        removeOnFail: {
+            age: 24 * 60 * 60,
+        },
+    },
+});
