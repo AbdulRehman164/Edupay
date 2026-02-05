e61017581762afb9f50517b47b3772d756d03105
Abdul Rehman <abdulrehmang571@gmail.com>
2026-02-05 11:10:14 +0500

    deleted unnecessary middleware in delete user endpoint

--- server/src/modules/admin/routes/users.route.js
+++ server/src/modules/admin/routes/users.route.js
@@ -6,14 +6,13 @@ import {
     deleteUserController,
 } from '../controllers/users.controller.js';
 import validateCreateUser from '../middlewares/validateCreateUser.middleware.js';
-import validateDeleteUser from '../middlewares/validateDeleteUser.middleware.js';
 
 const usersRoutes = Router();
 
 usersRoutes.get('/', searchUsersController);
 usersRoutes.get('/:id', getSingleUsersController);
 usersRoutes.post('/', validateCreateUser, createUserController);
-usersRoutes.delete('/:id', validateDeleteUser, deleteUserController);
+usersRoutes.delete('/:id', deleteUserController);
 //usersRoutes.patch('/:id', editUserController);
 
 export default usersRoutes;
